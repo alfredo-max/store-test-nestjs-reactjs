1898e8b6a50796ffb0fc738d9702933771e89794
alfredo.almarales <alfredo.almarales>
2025-05-10 20:27:16 -0500

    se cambia vpc para el lambda

--- back-nestjs-store/lambda.ts
+++ back-nestjs-store/lambda.ts
@@ -14,13 +14,27 @@ let cachedServer: Server;
 async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
     const expressApp = express();
+
     const nestApp = await NestFactory.create(
       AppModule,
       new ExpressAdapter(expressApp),
     );
     nestApp.use(eventContext());
+
     const configService = nestApp.get(ConfigService);
     const corsOrigin = configService.get<string>('CORS_ORIGIN');
+
+    expressApp.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', corsOrigin);
+      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+      if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+      }
+
+      next();
+    });
 
     nestApp.enableCors({ origin: corsOrigin });
     await nestApp.init();
--- back-nestjs-store/src/app.controller.ts
+++ back-nestjs-store/src/app.controller.ts
@@ -6,6 +6,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return "hola mundo";
+    return "test";
   }
 }
