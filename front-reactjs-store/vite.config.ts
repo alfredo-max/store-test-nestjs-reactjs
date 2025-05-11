23200463069e9d64d018e1912ac97baeef258beb
alfredo.almarales <alfredo.almarales>
2025-05-11 15:00:04 -0500

    vite config

--- front-reactjs-store/vite.config.ts
+++ front-reactjs-store/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitest/config' 
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
